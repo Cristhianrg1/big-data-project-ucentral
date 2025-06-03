7cbaa1e5f32747c351f73126332d8b80b45995c3
Cristhianrg1 <cristopher.rod.cr@gmail.com>
2025-06-02 22:41:12 -0500

    actualizacion proyecto

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ mongo_uri   = os.environ.get("MONGO_URI")
 
 if not mongo_uri:
     #uri = "mongodb+srv://DbCentral:DbCentral2025@cluster0.vhltza7.mongodb.net/?appName=Cluster0"
-    uri         = "mongodb+srv://DbCentral:DbCentral2025@cluster0.vhltza7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri         = "mongodb+srv://crrodriguezg:root@universidadcentralproje.j5z78y7.mongodb.net/?retryWrites=true&w=majority&appName=UniversidadCentralProject"
     mongo_uri   = uri
 
 # Función para conectar a MongoDB
